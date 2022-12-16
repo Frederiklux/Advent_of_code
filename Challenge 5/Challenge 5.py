95beafd670a78597b933ce93450c76cd36bdb873
Frederik Luxhøi <jb5738@jyskebank.dk>
2022-12-16 12:24:01 +0100

    Started Challenge 5

---
+++ Challenge 5/Challenge 5.py
@@ -0,0 +1,13 @@
+f = open('Challenge 5\input.txt', 'r')
+
+content = f.read()
+
+print(content)
+
+# Få loadet de 9 stacks i lister, hvor du starter fra bunden.
+
+# Lav en funktion, der flytter kasser som en kran efter kommandoer
+
+# Kør alle kommandoer igennem din kran-funktion
+
+# gg wp
