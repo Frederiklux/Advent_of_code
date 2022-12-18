5afe4c29adcb2467e92510f8320053d70abddf0b
Frederik Lux <Frederiklux@gmail.com>
2022-12-18 13:29:25 +0100

    Added max_lines

--- Challenge 5/Challenge 5.py
+++ Challenge 5/Challenge 5.py
@@ -2,6 +2,9 @@ f = open('input.txt', 'r')
 
 content = f.read()
 
+with open('input.txt', 'r') as fp:
+    max_lines = len(fp.readlines())
+
 all_stacks = []
 for j in range(9):
     curr_stack = ''
@@ -9,6 +12,8 @@ for j in range(9):
         curr_stack = curr_stack + content.splitlines()[i][1+(j)*4].strip()
     all_stacks.append(curr_stack)
 
+crane_commands = []
+for i in range len(content)
 # Få loadet de 9 stacks i lister, hvor du starter fra bunden.
 
 # Lav en funktion, der flytter kasser som en kran efter kommandoer
