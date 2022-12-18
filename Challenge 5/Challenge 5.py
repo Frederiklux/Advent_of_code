e5273a2e0cfeae6f181d96b6c5e17ef4cc57f799
Frederik Lux <Frederiklux@gmail.com>
2022-12-18 17:19:29 +0100

    Added crane function to Challenge 5

--- Challenge 5/Challenge 5.py
+++ Challenge 5/Challenge 5.py
@@ -10,10 +10,19 @@ for j in range(9):
     curr_stack = ''
     for i in range(8):
         curr_stack = curr_stack + content.splitlines()[i][1+(j)*4].strip()
-    all_stacks.append(curr_stack)
+    all_stacks.append(curr_stack[::-1])
 
 crane_commands = []
-for i in range len(content)
+for i in range(10,max_lines):
+    crane_commands.append(content.splitlines()[i])
+
+def crate_move(x):
+    moved_crates = x[5:7].strip()
+    from_stack = x[x.find('from')+5]
+    to_stack = x[x.find('to')+3]
+    print(moved_crates, from_stack, to_stack)
+
+crate_move(crane_commands[25])
 # Få loadet de 9 stacks i lister, hvor du starter fra bunden.
 
 # Lav en funktion, der flytter kasser som en kran efter kommandoer
