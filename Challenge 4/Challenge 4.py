341803f7eba7665fe2ea19fed45cec14e6d20fd1
Lux Laptop <Frederiklux@gmail.com>
2022-12-20 22:19:59 +0100

    Finished challenge 4

--- Challenge 4/Challenge 4.py
+++ Challenge 4/Challenge 4.py
@@ -4,7 +4,6 @@ f = open('input.txt', 'r')
 
 content = f.read()
 
-
 content = content.replace(",", ";")
 
 df = pd.DataFrame([x.split(';') for x in content.split('\n')])
@@ -27,47 +26,20 @@ for i in range(len(df)):
 
 sum(df['contained'])
 
-all_ranges_list = list(df['First_Elf']) + list(df['Second_Elf'])
-all_ranges = pd.DataFrame(all_ranges_list)
-all_ranges.rename(columns = {0:'Full_ranges'}, inplace = True)
-all_ranges[['lower', 'higher']] = all_ranges['Full_ranges'].str.split('-', expand=True)
+df['contained'] = False
+i=0
+for i in range(len(df)):
+    first_low = int(df['First_Elf'][i].split('-')[0])
+    first_high = int(df['First_Elf'][i].split('-')[1])
+    second_low = int(df['Second_Elf'][i].split('-')[0])
+    second_high = int(df['Second_Elf'][i].split('-')[1])
+    if (first_low >= second_low) and (first_low <= second_high):
+        df.loc[i, 'contained'] = True
+    if (first_high >= second_low) and (first_high <= second_high):
+        df.loc[i, 'contained'] = True
+    if (second_low >= first_low) and (second_low <= first_high):
+        df.loc[i, 'contained'] = True
+    if (second_high >= first_low) and (second_high <= first_high):
+        df.loc[i, 'contained'] = True
 
-counted = 0
-
-for i in range(len(all_ranges)):
-    for j in range(len(all_ranges)
-    all_ranges_temp = all_ranges.copy().drop(i)
-    if len(
-        all_ranges.loc[
-        (int(all_ranges.loc[i,'Full_ranges'].split('-')[0])>=all_ranges['lower'].astype(int)) & 
-        (int(all_ranges.loc[i,'Full_ranges'].split('-')[1])<=all_ranges['higher'].astype(int)),:]) > 1:
-        counted += 1
-
-all_ranges.loc[
-        (int(all_ranges.loc[i,'Full_ranges'].split('-')[0])>=all_ranges_temp['lower'].astype(int)),:]
-
-
-
-
-
-del all_ranges_temp[1]
-
-int(all_ranges.loc[i,'Full_ranges'].split('-')[0])
-int(all_ranges.loc[i,'Full_ranges'].split('-')[1])
-
-counted = 0
-
-filter(lambda counted: int(all_ranges[1].split('-')[0]) >= int(all_ranges_temp[1].split('-')[0]), counted)
-
-
-for i in range(len(all_ranges_list)):
-    for j in range(len(all_ranges_list)-1):
-        all_ranges_temp = all_ranges_list.copy()
-        del all_ranges_temp[j]
-        if (int(all_ranges_list[i].split('-')[0]) >= int(all_ranges_temp[j].split('-')[0])) and (int(all_ranges_list[i].split('-')[1]) <= int(all_ranges_temp[j].split('-')[1])):
-            counted += 1
-        else:
-            pass
-
-if (int(all_ranges[1].split('-')[0]) >= int(all_ranges_temp[2].split('-')[0])) and (int(all_ranges[i].split('-')[1]) <= int(all_ranges_temp[j].split('-')[1])):
-    counted += 1
+sum(df['contained'])
