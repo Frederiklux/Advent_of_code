f2936680b3e57037e6fc0c8e3cc797cc3b3ddb7a
Lux Laptop <Frederiklux@gmail.com>
2022-12-20 22:57:02 +0100

    Finished challenge 6

--- Challenge 6/Challenge 6.py
+++ Challenge 6/Challenge 6.py
@@ -1,18 +1,13 @@
-f = open('Challenge 6\input.txt', 'r')
+f = open('input.txt', 'r')
 
 content = f.read()
 
-print(content)
+for i in range(len(content)-14):
+    if len(set(content[i:i+4])) == 4:
+        print(i+4)
+        break
 
-
-
-count = []
-for i in range(len(content)-4):
-    if ((content[i] == content[i+1]) | (content[i] == content[i+2]) | (content[i] == content[i+3]) | (content[i] == content[i+4]) | 
-        (content[i+1] == content[i+2]) | (content[i+1] == content[i+3]) | (content[i+1] == content[i+4]) |
-        (content[i+2] == content[i+3]) | (content[i+2] == content[i+4]) | 
-        (content[i+3] == content[i+4])) == True:
-        pass
-    else: 
-        count.append(i) 
-print(count[0]+4)
+for i in range(len(content)-14):
+    if len(set(content[i:i+14])) == 14:
+        print(i+14)
+        break
