51790fee401dcda61e622c31a3c20722c9d3ec20
Frederik Lux <Frederiklux@gmail.com>
2023-12-24 10:35:30 +0100

    Day 2.1 done

--- 2023/Challenge 2/day2solution.sh
+++ 2023/Challenge 2/day2solution.sh
@@ -10,25 +10,49 @@ IFS=$'\n'
 
 sum=0
 
+truncate -s 0 games.txt 
+for VARIABLE in $value
+do
+    gamenumber=$(echo $VARIABLE | cut -d ':' -f 1 | tr -d [:alpha:])
+    echo "$gamenumber " >> games.txt
+done
+
 for VARIABLE in $value 
 do
     gamenumber=$(echo $VARIABLE | cut -d ':' -f 1 | tr -d [:alpha:])
-    echo $gamenumber
     red=$(echo $VARIABLE | cut -d ':' -f 2 | grep -oE '.{,3}red' | tr -d [:alpha:])
     for redballs in $red
     do
         if [ $redballs -le $redlimit ];
         then
-            echo "Alt er fint";
+            :
         else
-            echo "Alt er skidt";
+            grep -v "$gamenumber " games.txt > temp && mv temp games.txt
+            break
         fi;
     done;
-    echo $red
+    green=$(echo $VARIABLE | cut -d ':' -f 2 | grep -oE '.{,3}green' | tr -d [:alpha:])
+    for greenballs in $green
+    do
+        if [ $greenballs -le $greenlimit ];
+        then
+            :
+        else
+            grep -v "$gamenumber " games.txt > temp && mv temp games.txt
+            break
+        fi;
+    done;
+    blue=$(echo $VARIABLE | cut -d ':' -f 2 | grep -oE '.{,3}blue' | tr -d [:alpha:])
+    for blueballs in $blue
+    do
+        if [ $blueballs -le $bluelimit ];
+        then
+            :
+        else
+            grep -v "$gamenumber " games.txt > temp && mv temp games.txt
+            break
+        fi;
+    done;
 done
 
-#record game number DONE
-#separate hands in games
-#identify correct number with colours
-#check identified number with limit
-#add game number to sum in case above check succeeds
+echo "The answer to day 2 is: $(awk '{s+=$1} END {printf "%.0f\n", s}' games.txt)"
