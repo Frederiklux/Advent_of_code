14544eeaf01143bfb180e9d75a2f0029c23be032
Frederik Lux <Frederiklux@gmail.com>
2023-12-29 11:36:25 +0100

    Made output consice

--- 2023/Challenge 6/day6solution.sh
+++ 2023/Challenge 6/day6solution.sh
@@ -5,6 +5,7 @@ timelimits=($(sed '1q;d' "day6input.txt" | cut -d ':' -f 2 | awk '{$1=$1;print}'
 oldrecords=($(sed '2q;d' "day6input.txt" | cut -d ':' -f 2 | awk '{$1=$1;print}'))
 
 len="${#timelimits[@]}"
+waystowinproduct=0
 
 for (( i=0; i<$len; i++ )); 
 do 
@@ -21,10 +22,14 @@ do
             ((waystowin++))
         fi
     done
-    echo "Total ways to win is: " $waystowin
+    if [[ $waystowinproduct = 0 ]]
+    then
+        waystowinproduct=$waystowin
+    else waystowinproduct=$(($waystowinproduct*$waystowin))
+    fi
 done
 
-
+echo "The first answer to day 6 is:" $waystowinproduct
 
 #function functionName { echo "hello"; }
 
@@ -33,14 +38,14 @@ done
 currentlimit=$(sed '1q;d' "day6input.txt" | sed 's|[^0-9]||g')
 currentrecord=$(sed '2q;d' "day6input.txt" | sed 's|[^0-9]||g')
 
-for (( j=0; j<$currentlimit; j++ ));
-do
-    buttonhold=$j
-    sailtime=$(($currentlimit-$j))
-    traveldistance=$(($buttonhold*$sailtime))
-    if [[ $traveldistance -ge $currentrecord ]]
-    then 
-        ((waystowin++))
-    fi
-done
-echo "Total ways to win is: " $waystowin
+#for (( j=0; j<$currentlimit; j++ ));
+#do
+#    buttonhold=$j
+#    sailtime=$(($currentlimit-$j))
+#    traveldistance=$(($buttonhold*$sailtime))
+#    if [[ $traveldistance -ge $currentrecord ]]
+#    then 
+#        ((waystowin++))
+#    fi
+#done
+#echo "Total ways to win is: " $waystowin
